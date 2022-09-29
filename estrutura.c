053917e6aa293d97e5c43252f76a6bb0e90e0a24
Abram Grosman <abramgrossmann2016@gmail.com>
2022-09-28 22:13:34 -0300

    nomes

--- estrutura.c
+++ estrutura.c
@@ -1,6 +1,10 @@
 /* 
- * REPRESENTACAO DE GRAFOS - Versao 2022/1 
- * - Completado com fun��es desenvolvidas em aula
+ *Feito por 
+ *Abram Grossmann 31826131
+ *Lucas Quental 31852319 
+ *Amilton Costa 31876765
+ *Luis Felipe de Oliveira Lacerda e Silva 41922859
+ *Guilherme Naboa Ferreira 31876961
  */
 #include <stdio.h>
 #include <stdlib.h>
